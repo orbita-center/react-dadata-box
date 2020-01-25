3e02aef546054c070eeb2f9e24027c5018022413
ruvata <ruvata34@gmail.com>
2020-01-25 23:31:30 +0300

    closes #33 - implement customStyles property

--- index.d.ts
+++ index.d.ts
@@ -161,6 +161,12 @@ interface Props {
   className?: string;
   count?: number;
   customActions?: ((suggestions: DadataSuggestion[]) => React.ReactNode) | React.ReactNode;
+  customStyles?: {
+    'react-dadata__custom-action'?: string | React.CSSProperties;
+    'react-dadata__suggestion'?: string | React.CSSProperties;
+    'react-dadata__suggestion-note'?: string | React.CSSProperties;
+    'react-dadata__suggestions'?: string | React.CSSProperties;
+  };
   customEndpoint?: string | { host?: string; api?: string };
   customInput?: (props: BaseInputProps) => React.ReactNode;
   /**
